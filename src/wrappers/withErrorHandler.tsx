ebf8e75f9ec5710fd0ef5304939bb356f0fb6bf5
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-02-09 19:55:01 +0100

    :truck: Applied linting rules

--- src/wrappers/withErrorHandler.tsx
+++ src/wrappers/withErrorHandler.tsx
@@ -1,34 +1,30 @@
-import { NextApiHandler } from "next";
-import { ZodError } from "zod";
+import { NextApiHandler } from 'next';
 
-import ApiError from "@/class/ApiError";
+import { ZodError } from 'zod';
 
-import { checkIsHTTPMethod } from "@/util/api.util";
+import ApiError from '@/class/ApiError';
 
-import HTTPMethod from "@/constant/HTTPMethod";
+import { checkIsHTTPMethod } from '@/util/api.util';
+
+import HTTPMethod from '@/constant/HTTPMethod';
 
 /**
  * Next.js API Error Handler wrapper
  * @param handler The NextApiHandler route handle
  * @param method The allow method to access the route
  */
-const withErrorHandler = (
-  handler: NextApiHandler,
-  method?: HTTPMethod | HTTPMethod[],
-): NextApiHandler => {
+const withErrorHandler = (handler: NextApiHandler, method?: HTTPMethod | HTTPMethod[]): NextApiHandler => {
   return async (req, res) => {
     try {
       if (method) checkIsHTTPMethod(method)(req);
 
       return await handler(req, res);
     } catch (err) {
-      if (err instanceof ApiError)
-        return Response.json({ error: err.message }, { status: err.status });
+      if (err instanceof ApiError) return Response.json({ error: err.message }, { status: err.status });
 
-      if (err instanceof ZodError)
-        return Response.json({ error: err.message }, { status: 400 });
+      if (err instanceof ZodError) return Response.json({ error: err.message }, { status: 400 });
 
-      return Response.json({ error: "Server error." }, { status: 500 });
+      return Response.json({ error: 'Server error.' }, { status: 500 });
     }
   };
 };
